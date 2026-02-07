51103f39f9108a1f88122c08fa145e76a792ff71
Akishev Akzhan <akishevakzhan@gmail.com>
2026-02-07 22:55:19 +0500

    video binary

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/robloxscreenshot20250525_133332566.png')
-img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-img = cv2.GaussianBlur(img, (1, 1), 0)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture("videos/video_2026-02-07_19-13-01.mp4")
 
-img = cv2.Canny(img, 200, 200)
+while True:
+    success, img = cap.read()
 
-kernel = np.ones((5, 5), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
+    # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+    img = cv2.GaussianBlur(img, (1, 1), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.Canny(img, 100, 100)
 
-cv2.imshow('roblox', img)
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
 
-cv2.waitKey(0)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    cv2.imshow('Result', img)
+
+    if cv2.waitKey(1) & 0xFF == ord("x"):
+        break
+
 
 # cap = cv2.VideoCapture(0)
 # cap.set(3, 50000)
