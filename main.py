9ef90bb2bc27848111fb80828abd0fa1f886e65b
Akishev Akzhan <akishevakzhan@gmail.com>
2026-02-08 17:53:06 +0500

    rotate and transfrom an image

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("videos/video_2026-02-07_19-13-01.mp4")
+img = cv2.imread("images/RobloxScreenShot20250525_133332566.png")
 
-while True:
-    success, img = cap.read()
+def rotate(img_param, angle):
+    height, width = img.shape[:2]
+    point = (width // 2, height // 2)
 
-    # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-    img = cv2.GaussianBlur(img, (1, 1), 0)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    img = cv2.Canny(img, 100, 100)
-
-    kernel = np.ones((5, 5), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-
-    img = cv2.erode(img, kernel, iterations=1)
-
-    cv2.imshow('Result', img)
-
-    if cv2.waitKey(1) & 0xFF == ord("x"):
-        break
+    mat = cv2.getRotationMatrix2D(point, angle, 1)
+    return cv2.warpAffine(img_param, mat, (width, height))
 
 
+# img = rotate(img, 90)
+
+def transform(img_param, x, y):
+    mat = np.float32([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+
+img = transform(img, 30, 200)
+
+cv2.imshow("Out", img)
+cv2.waitKey(0)
 # cap = cv2.VideoCapture(0)
 # cap.set(3, 50000)
 # cap.set(4, 50000)
